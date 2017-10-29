e8b56725ea8c6df7df8c3445f50907a8b14692f6
Lucas Müller <lucasmullerm@gmail.com>
2017-10-29 06:23:14 -0200

    adding vs code

--- linux.sh
+++ linux.sh
@@ -3,6 +3,10 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 # grub customizer
 sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+# visual studio code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
 sudo apt update
 
@@ -18,3 +22,7 @@ sudo apt install ipython3 -y
 
 sudo apt install python-pip -ycd 
 sudo apt install ipython -y
+
+sudo apt install code
+
+
